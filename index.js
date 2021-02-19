586825fa982c481b80089243a5a7b68dd57b49a8
Kunal <kdshah6593@gmail.com>
2021-02-19 18:22:55 -0500

    commit tree 974580c5368f0620b24945316b92c983dedaba1c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    const url = "http://localhost:3000/users"
+    
+    let formData = {
+        name,
+        email
+    }
+    
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    
+    return fetch(url, configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+}
